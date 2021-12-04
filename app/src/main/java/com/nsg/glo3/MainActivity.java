9e5c581737cfc0f4fdbd430a17482a28b04182aa
nsg <blsso@naver.com>
2021-12-04 16:24:45 +0900

    sg

--- app/src/main/java/com/nsg/glo3/MainActivity.java
+++ app/src/main/java/com/nsg/glo3/MainActivity.java
@@ -1,53 +1,104 @@
 package com.nsg.glo3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
 
 
-    BottomNavigationView nav_view;
+
+    MeowBottomNavigation bottomNavigation;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nav_view = findViewById(R.id.nav_view);
+        bottomNavigation = findViewById(R.id.nav_view);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_baseline_chat_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_baseline_queue_music_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_baseline_live_tv_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.ic_baseline_auto_graph_24));
 
-        nav_view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
+            public void onShowItem(MeowBottomNavigation.Model item) {
 
+                Fragment fragment = null;
 
-                if (item.getItemId() == R.id.menu1) {
+                switch (item.getId()) {
+                    case 1:
+                        fragment = new main2_aichat();
+                        break;
+                    case 2:
+                        fragment = new main3_music();
+                        break;
+                    case 3:
+                        fragment = new main4_media();
+                        break;
+                    case 4:
+                        fragment = new main5_myprofile();
+                        break;
 
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new main2_aichat()).commit();
-                }else if (item.getItemId() == R.id.menu2) {
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new main3_music()).commit();
-                }else if (item.getItemId() == R.id.menu3) {
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new main4_media()).commit();
-                }else if (item.getItemId() == R.id.menu4) {
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new main5_myprofile()).commit();
                 }
 
-                return true;
+                loadFragment(fragment);
 
             }
         });
+
+        bottomNavigation.setCount(1, "10");
+
+        bottomNavigation.show(2, true);
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext(),
+                        "You Clicked" + item.getId(),
+                        Toast.LENGTH_SHORT).show();
+                
+            }
+        });
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext(),
+                        "You Reselected" + item.getId(),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout,fragment)
+                .commit();
+
+
     }
     //123123
 }
