fda2e59b35e0bcc06c1e19bed1d801f1080cebc3
곽호찬 <khc9154@naver.com>
2021-12-14 10:23:47 +0900

    호찬:메인 디자인 1차 수정. 리사이클러,어탭터,칼러xml 추가

--- app/src/main/java/com/nsg/glo3/main1_home.java
+++ app/src/main/java/com/nsg/glo3/main1_home.java
@@ -1,18 +1,27 @@
 package com.nsg.glo3;
 
 import android.content.SharedPreferences;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 
-public class main1_home extends Fragment {
 
+public class main1_home extends Fragment implements  adapterphone.ListItemClickListener {
+
+
+    RecyclerView phoneRecycler;
+    RecyclerView.Adapter adapter;
     TextView textView1,textView2;
     String[] weeks = {"당신은 멋진 사람입니다",
             "지치지 않고 잘 이겨내봅시다",
@@ -40,7 +49,36 @@ public class main1_home extends Fragment {
         textView1.setText("hello, "+ id + " 님"); //  출력
         textView2.setText(weeks[randomNum]);
 
-
+        phoneRecycler = view.findViewById(R.id.my_recycler);
+        phoneRecycler();
         return view;
+    }
+    private void phoneRecycler() {
+
+        //All Gradients
+        GradientDrawable gradient2 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xFFFFFFFF, 0xFFFFFFFF});
+        GradientDrawable gradient1 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xFFFFFFFF, 0xFFFFFFFF});
+        GradientDrawable gradient3 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xFFFFFFFF, 0xFFFFFFFF});
+        GradientDrawable gradient4 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xFFFFFFFF, 0xFFFFFFFF});
+
+
+        phoneRecycler.setHasFixedSize(true);
+        phoneRecycler.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        ArrayList<phonehelper> phonelocations = new ArrayList<>();
+        phonelocations.add(new phonehelper(gradient1, R.drawable.leehi1, "이하이 - 한숨"));
+        phonelocations.add(new phonehelper(gradient4, R.drawable.ui1, "IU - 이름에게"));
+        phonelocations.add(new phonehelper(gradient2, R.drawable.parkwon1, "박원 - 나"));
+        phonelocations.add(new phonehelper(gradient4, R.drawable.sondia1, "sondia - 어른"));
+        phonelocations.add(new phonehelper(gradient2, R.drawable.interstella1, "인터스텔라 - First Step"));
+
+
+        adapter = new adapterphone(phonelocations,this);
+        phoneRecycler.setAdapter(adapter);
+
+    }
+    @Override
+    public void onphoneListClick(int clickedItemIndex) {
+
     }
 }
